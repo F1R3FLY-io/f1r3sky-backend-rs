5305741b5a2146a6c325469546a5de71c6865269
beret <adam.manczuk@haloinvest.co.uk>
2025-04-17 20:50:15 +0100

    ignore JetBrains configuration, script to formating sources before commit

---
+++ format_changed_source.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Format all staged Rust files with rustfmt
+STAGED_RS_FILES=$(git diff --name-only | grep '\.rs$')
+echo "Staged Rust files: $STAGED_RS_FILES"
+
+if [ -n "$STAGED_RS_FILES" ]; then
+  echo "Formatting staged Rust files by $RUST_CMD..."
+  for file in $STAGED_RS_FILES; do
+    echo "Formatting Rust $file..."
+    rustfmt --style-edition 2024 "$file"
+    git add "$file"
+  done
+fi
+
+STAGED_TOML_FILES=$(git diff --name-only | grep '\.toml$')
+echo "Staged TOML files: $STAGED_TOML_FILES"
+
+if [ -n "$STAGED_TOML_FILES" ]; then
+  echo "Formatting staged TOML files  by $TAPLO_CMD..."
+  for file in $STAGED_TOML_FILES; do
+    echo "Formatting TOML $file..."
+    taplo fmt "$file"
+    git add "$file"
+  done
+fi
+
+exit 0
