c5e1c57fd7c65c71d46443004e557d6c80f920b7
beret <adam.manczuk@haloinvest.co.uk>
2025-04-23 10:31:33 +0200

    fix blocks

--- firefly-api/src/blocks.rs
+++ firefly-api/src/blocks.rs
@@ -24,11 +24,11 @@ impl BlocksClient {
     async fn api_get(
         &self,
         method: &str,
-        params: Option<&str>,
+        path_params: Option<&str>,
         args: Option<&str>,
     ) -> Result<Value, anyhow::Error> {
         let url = self.api_url(method);
-        let url = match params {
+        let url = match path_params {
             Some(params) => format!("{}/{}", url, params),
             None => url,
         };
@@ -64,7 +64,54 @@ impl BlocksClient {
         let url = self.api_url("blocks");
 
         // Create an HTTP client
+        // [
+        //     {
+        //         "blockHash": "64640917d50bec422b5b0eaec73152ac73605be46b87040d1c35962e86bdbd57",
+        //         "sender": "",
+        //         "seqNum": 0,
+        //         "sig": "",
+        //         "sigAlgorithm": "",
+        //         "shardId": "root",
+        //         "extraBytes": "",
+        //         "version": 1,
+        //         "timestamp": 1745250032590,
+        //         "headerExtraBytes": "",
+        //         "parentsHashList": [],
+        //         "blockNumber": 0,
+        //         "preStateHash": "9619d9a34bdaf56d5de8cfb7c2304d63cd9e469a0bfc5600fd2f5b9808e290f1",
+        //         "postStateHash": "7bab67cd2805e231536f3279c8c40654f522402954c889e10e9ca43cf1d268eb",
+        //         "bodyExtraBytes": "",
+        //         "bonds": [
+        //             {
+        //                 "validator": "04b103b9a8225589ce98d8417a3744f712e3b1660169e969297ed822e4edd88c13111726d35172ceb8a48065cfce5917292cbe42c8f48a73965100edb094c73365",
+        //                 "stake": 4
+        //             }
+        //         ],
+        //         "blockSize": "611829",
+        //         "deployCount": 10,
+        //         "faultTolerance": 1.0,
+        //         "justifications": [],
+        //         "rejectedDeploys": []
+        //     }
+        // ]
 
         let response = self.api_get("blocks", None, None).await?;
+
+        Ok(response
+            .as_array()
+            .and_then(|blocks| blocks.get(0))
+            .and_then(|block| block["blockHash"].as_str())
+            .map(|s| s.to_string())
+            .ok_or_else(|| anyhow!("Failed to extract block hash value."))?)
+    }
+
+    pub async fn get_blocks_values(&self, hash: String) -> Result<Vec<Value>, anyhow::Error> {
+        let response = self
+            .api_get("block", Some(&hash), None)
+            .await
+            .context("failed to get blocks")?;
+
+        let deploys = response["deploys"].as_array().context("missing deploys")?;
+        Ok(deploys.to_vec())
     }
 }
