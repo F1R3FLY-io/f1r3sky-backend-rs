36d499725e337d39b4e18dfcef8a4b508fdf2eed
beret <adam.manczuk@haloinvest.co.uk>
2025-04-28 19:39:03 +0200

    fix style

--- rsky-pds/src/apis/firefly/transactions.rs
+++ rsky-pds/src/apis/firefly/transactions.rs
@@ -10,8 +10,6 @@ pub async fn get_transactions(
     provider: &State<FireflyProvider>,
 ) -> Result<Json<Vec<Transaction>>, ApiError> {
     let client = provider.firefly();
-    let wallet_address = client.get_wallet_address()?;
-    let balance = client.get_balance().await.unwrap_or(0);
     let transactions = client.get_transactions().await?;
 
     Ok(Json(transactions))
